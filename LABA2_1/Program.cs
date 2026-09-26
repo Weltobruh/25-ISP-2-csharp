1d935c963fa35b7c1f4e88361cfb6ad7a4661701
Weltobruh <140549864+Weltobruh@users.noreply.github.com>
2026-09-26 12:00:28 +0200

    Add files via upload

---
+++ LABA2_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// 12 вариант Высокий
+try{
+    Console.Write("Введите х:");
+    double x = double.Parse(Console.ReadLine());
+    Console.Write("Введите у:");    
+    double y = double.Parse(Console.ReadLine());
+
+    Console.WriteLine((y<=1)&&(y>=x)&&(y>=-x));
+}
+catch(Exception ex){
+    Console.WriteLine(ex.Message);
+}
